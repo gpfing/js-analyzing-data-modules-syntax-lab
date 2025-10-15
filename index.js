5d78399d746ff6414916a4a6108a9efb5c3d4cc3
Garrett Pfingston <gpfingston@gmail.com>
2025-10-14 19:00:05 -0500

    Combined arrays

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+require('datejs');
 
+const users1 = ["garrett", "thomas", "chris"]
+const users2 = ["nick", "michael", "andy"]
+
+function combineUsers(...args){
+  let combineObject = {
+    users: [],
+  };
+
+  for (const ele of args){
+    combineObject.users.push(...ele);
+  }
+  combineObject.merge_date = new Date().toString("M/d/yyyy");
+  return combineObject;
+};
+
+console.log(combineUsers(users1, users2));
 
 
 module.exports = {
